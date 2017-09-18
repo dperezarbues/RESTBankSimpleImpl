d8d193fa1d2aff2b684685999e6483369c96e6da
David Perez <dpa@gmail.com>
2017-09-18 14:31:34 +0200

    Disabling HATEOAS

--- src/main/java/com/db/awmd/challenge/web/TransfersController.java
+++ src/main/java/com/db/awmd/challenge/web/TransfersController.java
@@ -6,18 +6,13 @@ import com.db.awmd.challenge.exception.InsufficientFundsException;
 import com.db.awmd.challenge.service.TransfersService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/v1/transfers")
@@ -33,45 +28,32 @@ public class TransfersController {
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   public @ResponseBody
-  ResponseEntity<Resource<Transfer>> executeTransfer(@RequestBody @Valid Transfer transfer) {
+  ResponseEntity<Transfer> executeTransfer(@RequestBody @Valid Transfer transfer) {
     log.info("Creating transfer {}", transfer);
 
     try {
       this.transfersService.createTransfer(transfer);
       this.transfersService.executeTransfer(transfer);
     } catch (InsufficientFundsException | AccountNotFoundException te) {
-      return new ResponseEntity<>(buildTransferResource(transfer), HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<>(transfer, HttpStatus.BAD_REQUEST);
     }
 
-    return new ResponseEntity<>(buildTransferResource(transfer), HttpStatus.CREATED);
+    return new ResponseEntity<>(transfer, HttpStatus.CREATED);
 
   }
 
   @GetMapping(path = "/{transferId}")
   public @ResponseBody
-  Resource<Transfer> getTransfer(@PathVariable Long transferId) {
+  Transfer getTransfer(@PathVariable Long transferId) {
     log.info("Retrieving transfer for id {}", transferId);
 
-    return buildTransferResource(this.transfersService.getTransfer(transferId));
+    return this.transfersService.getTransfer(transferId);
   }
 
   @GetMapping
   public @ResponseBody
-  List<Resource<Transfer>> getTransfers() {
-    List<Resource<Transfer>> resources = new ArrayList<>();
-    for (Transfer transfer : this.transfersService.getTransfer()) {
-      resources.add(buildTransferResource(transfer));
-    }
-    return resources;
+  List<Transfer> getTransfers() {
+    return this.transfersService.getTransfer();
   }
 
-
-  private Resource<Transfer> buildTransferResource(Transfer transfer) {
-    Resource<Transfer> resource = new Resource<>(transfer);
-    resource.add(linkTo(methodOn(TransfersController.class).getTransfer(transfer.getTransferId())).withSelfRel());
-    resource.add(linkTo(methodOn(AccountsController.class).getAccount(transfer.getReceiverAccountId())).withRel("receiver"));
-    resource.add(linkTo(methodOn(AccountsController.class).getAccount(transfer.getSenderAccountId())).withRel("sender"));
-
-    return resource;
-  }
 }
